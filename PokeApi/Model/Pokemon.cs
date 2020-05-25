10f75b57cd369a4cfcd0e03175ee4a36855ad611
Lucas Munoz <lucas00792@gmail.com>
2020-05-25 00:59:31 -0500

    Connected to PokeApi and gathered the information for "Squirtle"

---
+++ PokeApi/Model/Pokemon.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PokeApi.Model
+{
+    public class Pokemon
+    {
+        public string Name { get; set; }
+    }
+}
--- PokeApi/Program.cs
+++ PokeApi/Program.cs
@@ -1,12 +1,60 @@
-﻿using System;
-
+﻿using Newtonsoft.Json;
+using PokeApi.Model;
+using System;
+using System.IO;
+using System.Net;
 namespace PokeApi
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            PokeConnection pokeConnection = new PokeConnection();
+            pokeConnection.Connection();
+        }
+    }
+
+    public class PokeConnection
+    {
+        public void Connection()
+        {
+            string url = "https://pokeapi.co/api/v2/pokemon/squirtle/";
+            string pokemonJson = GetJson(url);
+            Console.WriteLine(pokemonJson);
+
+            string pokemonName = GetPokemonName(pokemonJson);
+            Console.WriteLine($"Name: {pokemonName}");
+        }
+
+        private string GetJson(string url)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            try
+            {
+                WebResponse response = request.GetResponse();
+                using (Stream responseStream = response.GetResponseStream())
+                {
+                    StreamReader reader = new StreamReader(responseStream, System.Text.Encoding.UTF8);
+                    return reader.ReadToEnd();
+                }
+            }
+            catch (WebException exc)
+            {
+                WebResponse errorResponse = exc.Response;
+                using (Stream responseStream = errorResponse.GetResponseStream())
+                {
+                    StreamReader reader = new StreamReader(responseStream, System.Text.Encoding.GetEncoding("utf-8"));
+                    string errorText = reader.ReadToEnd();
+                    Console.WriteLine($"There was an error: {Environment.NewLine}{errorText}");
+                }
+                throw;
+            }
+        }
+
+        private string GetPokemonName(string pokemonJson)
+        {
+            Pokemon pokemon = JsonConvert.DeserializeObject<Pokemon>(pokemonJson);
+            return pokemon.Name;
         }
     }
 }
